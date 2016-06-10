5c0cdf052e9b219167b023c7ac91830489bb52e5
Brent Martin <bmart35@gmail.com>
2016-06-09 22:29:15 -0500

    added a slideUp on document ready, and a slide toggle on click for education

--- style.js
+++ style.js
@@ -2,11 +2,16 @@ $(document).ready(function() {
   $('a.scroll').smoothScroll();
   $('#skills').find('.skills-expanded').slideUp();
   $('#jobs').find('.jobs-expanded').slideUp();
+  $('#education').find('.education-expanded').slideUp();
   $('#skills').on('click', function() {
     $(this).find('.skills-expanded').slideToggle();
   });
 
   $('#jobs').on('click', function() {
     $(this).find('.jobs-expanded').slideToggle();
+  });
+
+  $('#education').on('click', function() {
+    $(this).find('.education-expanded').slideToggle();
   });
 });
