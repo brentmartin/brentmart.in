14f750a04c798f01476870f6ea4b1d1d93b0fa90
Brent Martin <bmart35@gmail.com>
2016-06-09 22:28:40 -0500

    added a slideUp on document ready, and a slide toggle on click for skills

--- style.js
+++ style.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
   $('a.scroll').smoothScroll();
+  $('#skills').find('.skills-expanded').slideUp();
+  $('#skills').on('click', function() {
+    $(this).find('.skills-expanded').slideToggle();
+  });
 });
