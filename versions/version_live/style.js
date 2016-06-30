b6403038d6eaf54202e2a1d842abfe9ae628e442
Brent Martin <bmart35@gmail.com>
2016-06-30 10:28:58 -0500

    cleaned up JS, eliminating unused code for this version.

--- versions/version_live/style.js
+++ versions/version_live/style.js
@@ -1,15 +1,12 @@
 $(document).ready(function() {
   $('a.scroll').smoothScroll();
-  // $('.topic-read-more').add('topic-read-more');
   $('.topic').fadeOut();
   $('#skills').find('.skills-expanded').fadeOut();
   $('#jobs').find('.jobs-expanded').fadeOut();
   $('#education').find('.education-expanded').fadeOut();
   $('#results').find('.results-expanded').fadeOut();
   $('#interests').find('.interests-expanded').fadeOut();
-
   $('#textbox').find('.button-on').fadeOut();
-
 
 
 /*
@@ -22,40 +19,11 @@ SEE MORE BUTTON ON EACH TOPIC
     $(this).closest('.topic').find('.topic-collapse').toggle();
   });
 
-/*  performs the following on each specific topic:
-
-  $('#skills').find('.topic-button').on('click', function() {
-    $('#skills').find('.topic-button').toggleClass( 'button-off button-on' );
-    $('#skills').find('.skills-expanded').fadeToggle(250);
-  });
-
-  $('#skills').find('.topic-header').on('click', function() {
-    $('#skills').find('.topic-button').toggleClass( 'button-off button-on' );
-    $('#skills').find('.skills-expanded').fadeToggle(250);
-  });
-*/
-
-
 
 /*
-READ MORE/LESS
+NAVBAR BUTTONS
 ================================
-  $('#skills').find('.topic-read-more > .topic-read-link').on('click', function() {
-    $('#skills').find('.topic-button').toggleClass( 'button-off button-on' );
-    $('#skills').find('.topic-read-more').hide();
-    $('#skills').find('.topic-read-less').fadeIn(250);
-    $('#skills').find('.skills-expanded').fadeIn(250);
-  });
-
-  $('#skills').find('.topic-read-less > .topic-read-link').on('click', function() {
-    $('#skills').find('.topic-button').toggleClass( 'button-off button-on' );
-    $('#skills').find('.topic-read-less').fadeOut(250);
-    $('#skills').find('.topic-read-more').delay(250).fadeIn(250);
-    $('#skills').find('.skills-expanded').fadeOut(250);
-  });
 */
-
-
   $('#textbox').find('li:nth-child(1)').on('click', function() {
     $(this).toggleClass( 'button-off button-on' );
     $(this).closest('#textbox').find('#skills').fadeToggle(250);
@@ -83,13 +51,13 @@ READ MORE/LESS
   });
 
 
+/*
+FIXED NAVBAR ON SCROLL
+================================
+*/
 
   $(window).scroll(function () {
-
       var posTop = $(window).scrollTop() - $('.nav-space').offset().top
-
-    console.log('from .nav-space: '+posTop+' | from top of page: '+$(window).scrollTop());
-
     if (posTop > 0) {
       $('#nav_bar').addClass('navbar-fixed');
     }
